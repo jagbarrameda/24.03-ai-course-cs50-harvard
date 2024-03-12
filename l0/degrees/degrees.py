3e3237d8fc2dea132886b56670bdb4f685bbe9c7
Jose Barrameda <jagbarrameda@gmail.com>
2024-03-12 15:19:37 -0400

    Solved project l0 degrees

---
+++ l0/degrees/degrees.py
@@ -0,0 +1,179 @@
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+from collections import deque
+from typing import Deque
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+    # directory = '/Users/jbarrameda/Library/CloudStorage/GoogleDrive-jagbarrameda@gmail.com/My Drive/documents/24.03-ai-course-cs50-harvard/l0/degrees/small'
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+    if source == target:
+        return []
+
+    frontier = deque() # of type Node
+    visitedStars = set()
+
+    # init frontier
+    visitedStars.add(source)
+    for curr_neighbor in neighbors_for_person(source):
+        if curr_neighbor[1] in visitedStars:
+            continue
+        node = Node(curr_neighbor, None, None)
+        frontier.append(node)
+
+    # traverse
+    while len(frontier) > 0:
+        node: Node = frontier.popleft()
+        curr_neighbor = node.state # movie-person pair
+        person = curr_neighbor[1]
+        if person == target:
+            return getPath(node)
+        # add to the frontier if not visited
+        if person in visitedStars:
+            continue
+        visitedStars.add(person)
+        for newNeighbor in neighbors_for_person(person):  # movie-person pair
+            if newNeighbor[1] in visitedStars:
+                continue
+            newNode = Node(newNeighbor, node, None)
+            frontier.append(newNode)
+    return None
+
+def getPath(endNode: Node) -> list: # list of movie-person pairs
+    path = []
+    node = endNode
+    while node is not None:
+        state = node.state # movie-person pair
+        path.insert(0, state) # insert at the beginning
+        node = node.parent
+    return path
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
